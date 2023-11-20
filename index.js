ce98206d21d2a45525ca0250a172a0b6a50002a9
William Jamieson <jamiewilliamson@protonmail.com>
2023-11-19 20:49:30 -0500

    Added "about me" page, dark mode button w/ cookies

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
+let lightMode = getMode();
 let modeSwitcher = document.getElementById("modeSwitcherOne")
-// let pageBody = document.getElementBy
-let lightMode = true
+if (lightMode) {
+    document.body.id = "lightBody"
+}
+else {
+    document.body.id = "darkBody"
+    modeSwitcher.id = "modeSwitcherTwo"
+}
 
 modeSwitcher.addEventListener("click", ()=> {
-    lightMode = !lightMode
+    lightMode = !lightMode;
+    setMode(lightMode);
+    setCookie(lightMode);
+})
+
+function getMode() {
+    let cookies = document.cookie;
+    cookies = cookies.split(";");
+    for (i = 0; i < cookies.length; i++) {
+        if (cookies[i].includes("lightMode")) {
+            mode = cookies[i].split("=")[1];
+            if (mode === "off") {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+    return true;
+}
+
+function setMode(lightMode) {
     if (!lightMode) {
-        document.body.style.background = "black";
-        document.body.style.color = "white"
-        modeSwitcher.id = "modeSwitcherTwo"
+        document.body.id = "darkBody";
+        modeSwitcher.id = "modeSwitcherTwo";
     }
     else {
-        document.body.style.background = "white";
-        document.body.style.color = "black"
-        modeSwitcher.id = "modeSwitcherOne"
+        document.body.id = "lightBody";
+        modeSwitcher.id = "modeSwitcherOne";
     }
+}
 
-
-})
+function setCookie(lightMode) {
+    if (!lightMode) {
+        document.cookie = "lightMode=off; path=/";
+    }
+    else {
+        document.cookie = "lightMode=on; path=/";
+    }
+}
