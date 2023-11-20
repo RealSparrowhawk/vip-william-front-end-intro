228456bfe8a9fb410451b80949217878b2fbddd1
William Jamieson <jamiewilliamson@protonmail.com>
2023-11-19 20:57:07 -0500

    Added comments

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let lightMode = getMode();
+let lightMode = getMode(); // Used to tell if page is in light or dark mode
+// Grabbing mode switcher button so we can change its style as well
 let modeSwitcher = document.getElementById("modeSwitcherOne")
+// Initially sets document to either light or dark mode based on settings from cookies
 if (lightMode) {
     document.body.id = "lightBody"
 }
@@ -8,12 +10,17 @@ else {
     modeSwitcher.id = "modeSwitcherTwo"
 }
 
+// Adds action listening to button, updates the "mode" variable and the cookies to reflect new page state
 modeSwitcher.addEventListener("click", ()=> {
     lightMode = !lightMode;
     setMode(lightMode);
     setCookie(lightMode);
 })
 
+/*
+Function that checks the page's cookies for the "lightMode" keyword, which will be either on or off. Returns false
+if off, true if on (and true if no cookies have been set, as page defaults to light mode)
+ */
 function getMode() {
     let cookies = document.cookie;
     cookies = cookies.split(";");
@@ -31,6 +38,7 @@ function getMode() {
     return true;
 }
 
+// Function that changes styles of body and the dark mode button once page state has been changed.
 function setMode(lightMode) {
     if (!lightMode) {
         document.body.id = "darkBody";
@@ -42,6 +50,7 @@ function setMode(lightMode) {
     }
 }
 
+// Function that sets the appropriate lightMode on or off cookie.
 function setCookie(lightMode) {
     if (!lightMode) {
         document.cookie = "lightMode=off; path=/";
